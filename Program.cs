7a487421a5968887e18cf70fec67ee365955240a
Hazal Sendogan <sendoganhazal@gmail.com>
2026-03-31 14:56:45 +0300

    if, else if ve else ile nested karar yapısı

--- Program.cs
+++ Program.cs
@@ -16,28 +16,64 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-// Doubles bonus
+// // Doubles bonus
 
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+// }
+
+// // Triples bonus
+// if ((roll1 == roll2) && (roll2 == roll3)) 
+// {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+// }
+//Nested if statements to check for doubles and triples without double counting bonuses:
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-}
-
-// Triples bonus
-if ((roll1 == roll2) && (roll2 == roll3)) 
-{
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    //Triples bonus
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    //Doubles bonus
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+    Console.WriteLine($"Your total including the bonus: {total}");
 }
 
 //Winning condition: If the sum of the three dice rolls, plus any point bonuses, is 15 or greater, you win the game. Otherwise, you lose.
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
-}
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// }
 
-if (total < 15)
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+
+//Winning condition using if-else statement:
+if (total >= 16)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
